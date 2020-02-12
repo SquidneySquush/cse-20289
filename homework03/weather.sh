7eecf3e3310c79cf168ef5977a34b2bdcd08d792
Sydney Hnat <shnat@nd.edu>
2020-02-12 13:25:09 -0500

    homework03

--- homework03/weather.sh
+++ homework03/weather.sh
@@ -3,7 +3,7 @@
  # Globals
 URL="https://forecast.weather.gov/sipcity.php"
 ZIPCODE=46556
-forecast="NA"
+forecast=0
 CELSIUS=0
 
 # Functions
@@ -26,28 +26,35 @@ weather_information() {
 
 temperature() {
     # Extract temperature information from weather source
-    if [ $CELSIUS -eq 1 ]; then
-    weather_information | grep "myforecast-current-sm" | grep -Eo "[[:digit:]]+"
+    if [ "$CELSIUS" -eq 0 ]; then  # If celcius = 0 then search for temp in F
+      weather_information | grep "myforecast-current-lrg" | grep -Eo "(-)*+[[:digit:]]+"
     else
-    weather_information | grep "myforecast-current-lrg" | grep -Eo "[[:digit:]]+"
+      weather_information | grep "myforecast-current-sm" | grep -Eo "(-)*+[[:digit:]]+"
     fi
 }
 
 forecast() {
     # Extract forecast information from weather source
-    weather_information | grep "myforecast-current" | grep -Eo "[[:blank:]],[[:word:]]"| sed -e 's/^\s*//' | sed 's/<p class="myforecast-current">//' | sed 's/<\/p>//'
+    weather_information | grep "myforecast-current" | grep -Eo "[[:word:]]+[[:punct:]]+"| sed -e 's/^\s*//' | sed 's/<p class="myforecast-current">//' | sed 's/<\/p>//'
 }
 
 # Parse Command Line Options
 
 while [ $# -gt 0 ]; do
     case $1 in
-        -h) usage 0 ;;
-        -f) echo "Forecast:    $(forecast)";;
-        -c) CELSIUS=1 ;;
+        -h)
+            usage 0 ;;
+        -f)
+            echo "Forecast: $forecast";;
+        -c)
+            export CELSIUS=1;;
       esac
     shift
 done
+
+if test "$#" -ne 1; then
+  ZIPCODE=$1
+fi
 
 # Display Information
 
